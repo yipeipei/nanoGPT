update train.py, fix gradient clipping for FSDP1

train.py
@@ -309,7 +309,10 @@ while True:
     # clip the gradient
     if grad_clip != 0.0:
         scaler.unscale_(optimizer)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+        if isinstance(model, FSDP):
+            model.clip_grad_norm_(grad_clip)
+        else:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
     # step the optimizer and scaler if training in fp16
     scaler.step(optimizer)
     scaler.update()