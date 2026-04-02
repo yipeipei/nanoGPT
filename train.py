144bc9a4d135e2b858bfb01c2e3e846c1f0c218f
YI Peipei <dev@yipeipei.com>
2026-04-02 15:18:00 +0800

    update train.py, fix raw_model for PP

--- train.py
+++ train.py
@@ -345,7 +345,7 @@ X, Y = get_batch('train') # fetch the very first batch
 t0 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
 # raw_model = model.module if ddp else model # unwrap DDP container if needed
-raw_model = model if isinstance(model, GraphModule) else model.module
+raw_model = model # PP
 running_mfu = -1.0
 while True:
 
