547a3dc3806ae30ea7eaa984b68b0457efa89eb5
YI Peipei <dev@yipeipei.com>
2026-04-08 11:14:39 +0800

    update install.sh, install deps

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ set -o errexit  # Exit immediately if a command exits with a non-zero status.
 set -o xtrace  # Print commands and their arguments as they are executed.
 
 ####### END-OF-TEMPLATE
+
+pip install torch numpy transformers datasets tiktoken wandb tqdm
