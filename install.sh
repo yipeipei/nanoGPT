42efb54201b99acb254f972ffa4632ecd56cb8e4
YI Peipei <dev@yipeipei.com>
2026-04-08 11:11:59 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -o errexit  # Exit immediately if a command exits with a non-zero status.
+set -o xtrace  # Print commands and their arguments as they are executed.
+
+####### END-OF-TEMPLATE
