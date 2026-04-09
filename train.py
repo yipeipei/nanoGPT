48d318a4928edf0ffbe78d47381f2188c80ed885
YI Peipei <dev@yipeipei.com>
2026-04-09 10:54:54 +0800

    update train.py, fix tokens_per_iter

--- train.py
+++ train.py
@@ -175,7 +175,8 @@ else:
     master_process = True
     seed_offset = 0
     ddp_world_size = 1
-tokens_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * block_size
+# tokens_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * block_size
+tokens_per_iter = gradient_accumulation_steps * batch_size * block_size
 print(f"tokens per iteration will be: {tokens_per_iter:,}")
 
 if master_process:
