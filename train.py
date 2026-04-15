681d2c0ffc8baa03c18e24c0314d4013ecee4814
YI Peipei <dev@yipeipei.com>
2026-04-15 16:43:07 +0800

    update train.py, estimate mfu at last stage

--- train.py
+++ train.py
@@ -458,7 +458,7 @@ while True:
     t1 = time.time()
     dt = t1 - t0
     t0 = t1
-    if iter_num % log_interval == 0 and master_process:
+    if iter_num % log_interval == 0 and stage.is_last:
         # get loss as float. note: this is a CPU-GPU sync point
         # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
         lossf = loss.item() * gradient_accumulation_steps
