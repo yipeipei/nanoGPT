5efbffab1900757114a5056dbfba55793427f406
YI Peipei <dev@yipeipei.com>
2026-04-21 15:30:51 +0800

    update train.py and model.py, add config for flash attention

--- model.py
+++ model.py
@@ -42,7 +42,10 @@ class CausalSelfAttention(nn.Module):
         self.n_embd = config.n_embd
         self.dropout = config.dropout
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if config.flash:
+            self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        else:
+            self.flash = False
         if not self.flash:
             print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
             # causal mask to ensure that attention is only applied to the left in the input sequence
@@ -114,6 +117,7 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    flash: bool = True
 
 class GPT(nn.Module):
 
