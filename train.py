d0b930bac18b22ce0057e8799bcd8da7d315e3d5
YI Peipei <dev@yipeipei.com>
2026-04-21 15:43:22 +0800

    update train.py, log config

--- train.py
+++ train.py
@@ -153,6 +153,7 @@ compile = True # use PyTorch 2.0 to compile the model to be faster
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
 exec(open('configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
+print('config:', config)
 # -----------------------------------------------------------------------------
 
 # various inits, derived attributes, I/O setup
